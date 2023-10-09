8ae23e1a8f04d0a0ceeaf4a4a0bb2d0c812bc6e3
Sam Ichida <84211012+i-am-schida@users.noreply.github.com>
2023-10-09 16:01:17 -0700

    addition: arithmetic function
    arithmetic function added.
    +/- button added.

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ const outputDisplay = document.getElementById('output');
 const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
 const clearButton = document.getElementById('clear');
+const equalButton = document.getElementById('calculate');
+const plusMinusButton = document.getElementById('pos-neg');
 
 // VARIABLES
 let currentInput = '0';
-
+let firstOperand = null;
+let selectedOperator = null;
 
 // FUNCTIONS:
 
@@ -18,7 +21,36 @@ function updateDisplay() {
 // Function to clear the result display
 function clearDisplay () {
   currentInput = '0';
+  firstOperand = null;
+  selectedOperator = null;
   updateDisplay();
+}
+
+// Function for arithmetic operations
+function performCalculation() {
+  if (firstOperand !== null && selectedOperator !== null) {
+    const secondOperand = parseFloat(currentInput);
+    switch (selectedOperator) {
+      case '+':
+        currentInput = (firstOperand + secondOperand).toString();
+        break;
+      case '-':
+        currentInput = (firstOperand - secondOperand).toString();
+        break;
+      case '*':
+        currentInput = (firstOperand * secondOperand).toString();
+        break;
+      case '/':
+        if (secondOperand === 0) {
+          currentInput = 'Error';
+        } else {
+          currentInput = (firstOperand / secondOperand).toString();
+        }
+        break;
+    }
+    firstOperand = parseFloat(currentInput);
+    selectedOperator = null;
+  }
 }
 
 // EVENT LISTENERS:
@@ -41,11 +73,31 @@ numberButtons.forEach((button) => {
 operatorButtons.forEach((button) => {
   button.addEventListener('click', () => {
     const operator = button.textContent;
-    currentInput = operator;
+
+    if (selectedOperator !== null) {
+      performCalculation();
+    }
+
+    firstOperand = parseFloat(currentInput);
+    selectedOperator = operator;
+    currentInput = '';
     updateDisplay();
   });
+});
+// equal button
+equalButton.addEventListener('click', () => {
+  performCalculation();
+  updateDisplay();
 });
 // clear button
 clearButton.addEventListener('click', () => {
   clearDisplay();
+});
+// plus or minus button
+plusMinusButton.addEventListener('click', () => {
+  
+  if (!isNaN(currentInput)) {
+    currentInput = (-parseFloat(currentInput)).toString();
+    updateDisplay();
+  }
 });
